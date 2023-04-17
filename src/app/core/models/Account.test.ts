ace2a71f790bc3578f1e34a43ee0e93d4e03941e
Nga Bui <nga.bui@supremetech.vn>
2023-04-17 08:38:14 +0700

    mock localStorage

--- src/app/core/models/Account.test.ts
+++ src/app/core/models/Account.test.ts
@@ -6,7 +6,6 @@ describe('test Account class', () => {
   describe('login', () => {
     it('login is called', () => {
       const setTokenSpy = jest.spyOn(AuthStorageService.prototype, 'setToken');
-      setTokenSpy.mockImplementation((token: any) => token);
       account.login();
       expect(setTokenSpy).toBeCalledTimes(1);
       expect(setTokenSpy).toBeCalledWith('token');
@@ -15,7 +14,6 @@ describe('test Account class', () => {
   describe('logout', () => {
     it('logout is called', () => {
       const removeTokenSpy = jest.spyOn(AuthStorageService.prototype, 'removeToken');
-      removeTokenSpy.mockImplementation();
       account.logout();
       expect(removeTokenSpy).toBeCalledTimes(1);
       expect(removeTokenSpy).toBeCalledWith();
@@ -24,7 +22,6 @@ describe('test Account class', () => {
   describe('isAuthenticated', () => {
     it('isAuthenticated is called', () => {
       const getTokenSpy = jest.spyOn(AuthStorageService.prototype, 'getToken');
-      getTokenSpy.mockImplementation();
       account.isAuthenticated();
       expect(getTokenSpy).toBeCalledTimes(1);
       expect(getTokenSpy).toBeCalledWith();
---
+++ src/app/core/services/authStorage.service.test.ts
@@ -0,0 +1,30 @@
+import { AuthStorageService } from './authStorage.service';
+
+describe('test auth service', () => {
+  const authStorageService = new AuthStorageService();
+  jest.spyOn(Storage.prototype, 'getItem');
+  Storage.prototype.getItem = jest.fn();
+  it('getToken is called', () => {
+    jest.spyOn(Storage.prototype, 'getItem');
+    Storage.prototype.getItem = jest.fn();
+    authStorageService.getToken();
+    expect(localStorage.getItem).toBeCalledTimes(1);
+    expect(localStorage.getItem).toBeCalledWith('token');
+  });
+
+  it('setToken is called', () => {
+    jest.spyOn(Storage.prototype, 'setItem');
+    Storage.prototype.setItem = jest.fn();
+    authStorageService.setToken('token');
+    expect(localStorage.setItem).toBeCalledTimes(1);
+    expect(localStorage.setItem).toBeCalledWith('token', 'token');
+  });
+
+  it('removeToken is called', () => {
+    jest.spyOn(Storage.prototype, 'removeItem');
+    Storage.prototype.removeItem = jest.fn();
+    authStorageService.removeToken();
+    expect(localStorage.removeItem).toBeCalledTimes(1);
+    expect(localStorage.removeItem).toBeCalledWith('token');
+  });
+});
